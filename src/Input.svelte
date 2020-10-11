f87252385b84b84daa7534353c04d8665dd32b60
pngwn <hello@pngwn.io>
2020-10-11 01:21:02 +0100

    add simple tabs to switch active component

--- src/Input.svelte
+++ src/Input.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
+	import Tabs from "./Tabs.svelte";
+
 	import type { Component } from "./types";
 
 	export let components: Component[] = [];
 	export let current: number = 0;
 
 	$: current_component = components.findIndex(({ id }) => id === current);
+	$: tabs = components.map(({ id, name, type }) => ({ id, name, type }));
 </script>
 
 <section>
+	<Tabs {tabs} {current} on:select={({ detail }) => (current = detail)} />
 	<textarea bind:value={components[current_component].source} />
 </section>
---
+++ src/Tabs.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { Tab } from "./types";
+
+	import { createEventDispatcher } from "svelte";
+
+	const dispatch = createEventDispatcher<{ select: number }>();
+
+	export let tabs: Tab[] = [];
+	export let current: number = 0;
+</script>
+
+<ul>
+	{#each tabs as { name, type, id }}
+		<li class:active={id === current} on:click={() => dispatch('select', id)}>
+			{name}.{type}
+		</li>
+	{/each}
+</ul>
+
+<style>
+	.active {
+		font-weight: 900;
+		text-decoration: underline;
+	}
+</style>
