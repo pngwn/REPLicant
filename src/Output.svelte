9709717a52577d050a7eec5a53ba6c6ddbaf5196
pngwn <hello@pngwn.io>
2020-10-11 15:05:53 +0100

    pass bundle to iframe, evaluate and render

--- src/Output.svelte
+++ src/Output.svelte
@@ -15,8 +15,22 @@
 	<head>
 		<script type="module">
 
+			let c;
+
+			function update(source) {
+				const blob = new Blob([source], { type: 'text/javascript' });
+				const url = URL.createObjectURL(blob);
+
+				import(url).then(({ default: App }) => {
+					if (c) c.$destroy();
+
+					document.body.innerHTML = '';
+					c = new App({ target: document.body })
+				})
+			}
+
 			window.addEventListener('message', event => {
-				console.log(event.data)
+				update(event.data)
 			}, false)
 		<\/script>
 	</head>
