28f54dbd8af541374427b61d643b1729b88a4dad
pngwn <hello@pngwn.io>
2020-10-11 14:31:28 +0100

    correctly resolve local repl imports

--- src/worker.ts
+++ src/worker.ts
@@ -3,15 +3,24 @@ import type { Component } from "./types";
 import * as rollup from "rollup/dist/es/rollup.browser.js";
 
 const CDN_URL = "https://cdn.jsdelivr.net/npm";
+importScripts(`${CDN_URL}/svelte/compiler.js`);
+
+const component_lookup: Map<string, Component> = new Map();
 
 async function fetch_package(url: string): Promise<string> {
 	return (await fetch(url)).text();
 }
 
+function generate_lookup(components: Component[]): void {
+	components.forEach((component) => {
+		component_lookup.set(`./${component.name}.${component.type}`, component);
+	});
+}
+
 self.addEventListener(
 	"message",
 	async (event: MessageEvent<Component[]>): Promise<void> => {
-		console.log(event.data);
+		generate_lookup(event.data);
 
 		const bundle = await rollup.rollup({
 			input: "./App.svelte",
@@ -31,16 +40,27 @@ self.addEventListener(
 							if (resolved.endsWith(".mjs")) return resolved;
 							return `${resolved}/index.mjs`;
 						}
+
+						// repl components
+						if (component_lookup.has(importee)) return importee;
 					},
 					async load(id: string) {
+						if (component_lookup.has(id))
+							return component_lookup.get(id).source;
+
 						return await fetch_package(id);
 					},
-					transform(code: string, id: string) {},
+					transform(code: string, id: string) {
+						//@ts-ignore
+						if (/.*\.svelte/.test(id)) return svelte.compile(code).js.code;
+					},
 				},
 			],
 		});
 
 		const output: string = (await bundle.generate({ format: "esm" })).output[0]
 			.code;
+
+		self.postMessage(output);
 	}
 );
